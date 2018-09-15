519ac2f069462c4b750879c7b4ec48de76861662
Clarence Chan <cchan@live.ca>
2018-09-14 21:59:52 -0400

    Updated readme

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ class Main extends Component {
           <p>
             The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
           The "game" is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves, or, for advanced "players", by creating patterns with particular properties.
-          <br /><a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Wikipedia</a>
+          <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Wikipedia</a>
           </p>
           <h2>Rules</h2>
           <ol>
